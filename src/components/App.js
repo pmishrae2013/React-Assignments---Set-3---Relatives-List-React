df2aa4384253c777517db91c976908b6192b5819
Purushottam <85189176+pmishrae2013@users.noreply.github.com>
2021-08-08 17:24:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(){
+        this.state= {Relation : [{relatives:"Ram", index:"relativeListItem1"}, 
+            {relatives:"Shyam", index:"relativeListItem2"},
+            {relatives:"Laxman", index:"relativeListItem3"},
+            {relatives:"sita", index:"relativeListItem4"},
+            {relatives:"Ravan", index:"relativeListItem5"}
+        ]}
+    }
     render() {
-
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <ol>{
+                   this.state.Relation.map((rel)=> {
+                       <li key={rel.index}>{rel.name}</li>
+                   })
+                   }</ol>
             </div>
         )
     }
